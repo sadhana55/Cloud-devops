d3dffe16e095bbaca9d19b0a8cbe34ffa485ff54
sadhana55 <sadhanananhelal.singh@mavs.uta.edu>
2018-04-21 14:59:52 -0500

    intial commit

---
+++ src/main/java/com/devopsstart/DevopsStartApplication.java
@@ -0,0 +1,12 @@
+package com.devopsstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsStartApplication.class, args);
+	}
+}
